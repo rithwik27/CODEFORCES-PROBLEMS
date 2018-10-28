589f244b7c336470c2e84745b40ef2d6440bc88e
Rithwik Srikakolapu <32234083+rithwik27@users.noreply.github.com>
2018-10-28 23:03:37 +0530

    Update dubstep.py
    efficient code

--- python sums/dubstep.py
+++ python sums/dubstep.py
@@ -1,6 +1,4 @@
-m = list(map(str, input().split("WUB")))
-k = m.count("")
-if k!=0:
-    for i in range(k):
-        m.remove("")
-print(" ".join(m))
+n=list(input().split("WUB"))
+for i in n:
+    if (i != ''):
+        print(i,end=" ")
